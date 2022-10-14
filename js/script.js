3abe62f56a05b58a60f87d928201c9a3f23fcdaa
MidnightRoam <saldatsenka@list.ru>
2022-10-14 17:26:30 +0200

    Final of a popup window were developed

--- js/script.js
+++ js/script.js
@@ -45,38 +45,93 @@
 //         });
 //     }
 // }
-const openPopUp = document.getElementById('open_pop_up');
-const closePopUp = document.getElementById('pop_up_close');
-const popUp = document.getElementById('pop_up');
+// const openPopUp = document.getElementById('open_pop_up');
+// const closePopUp = document.getElementById('pop_up_close');
+// const popUp = document.getElementById('pop_up');
+//
+// openPopUp.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     popUp.classList.add('active');
+// })
+//
+// closePopUp.addEventListener('click', () => {
+//     popUp.classList.remove('active');
+// })
 
-openPopUp.addEventListener('click', function (e) {
-    e.preventDefault();
-    popUp.classList.add('active');
-})
-
-closePopUp.addEventListener('click', () => {
-    popUp.classList.remove('active');
-})
-
-/* Функция получения данных пользователя с бэкенда */
-async function getResponse() {
-    let response = await fetch('http://127.0.0.1:3000')
-    let content = await response.json()
-
+// Функция получения данных пользователя с бэкенда и вывод пользотвательских карточек
+async function getUsers() {
+    let response = await fetch('http://127.0.0.1:3000');
+    let content = await response.json();
     let user_list = document.querySelector('.page')
 
     let key;
     for (key in content) {
         /* Добавляем в HTML персональные карточки пользователей */
-        user_list.innerHTML += `          
+        user_list.innerHTML += ` 
             <div class="card__body mg-10 rounded shadow popup-link">
                 <div class="user__name pd-10">${content[key].name}</div>
                 <div class="user__info pd-10">
                 <div class="user__phone">${content[key].phone}</div>
                 <div class="user__email">${content[key].email}</div>
-            </div>`
+            </div>
+        `
+        user_list.addEventListener("click", () => openModal(content[key].name))
     }
 }
-console.log(getResponse());
+console.log(getUsers());
 
+// Модальное окно
+const modalEl = document.querySelector('.modal');
 
+// Функционал открытия модального окна и получения данных пользователей
+async function openModal(id) {
+    console.log(id)
+    let response = await fetch(`http://127.0.0.1:3000`);
+    let content = await response.json();
+
+    // При клике на карточку делаем модальное окно видимое и запрещаем скорллинг
+    modalEl.classList.add('modal--show');
+    document.body.classList.add("stop-scrolling");
+
+        modalEl.innerHTML = `
+        <div class="modal__card rounded">
+            <button type="button" class="modal__button-close">&#10006;</button>
+            <div class="user__name"></div>
+            <div class="user__phone">Телефон: </div>
+            <div class="user__email">Почта: </div>
+            <div class="user__date">Дата приема: </div>
+            <div class="user__position-name">Должность: </div>
+            <div class="user__department">Подразделение: </div>
+            <div class="card__info">
+                <p><b>Дополнительная информация:</b></p>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam aperiam assumenda delectus 
+                distinctio harum neque numquam porro repudiandae sed? Eos numquam porro provident recusandae!
+                </p>
+            </div>
+        </div>   
+
+    `
+    const btnClose = document.querySelector(".modal__button-close")
+    btnClose.addEventListener("click", () => closeModal());
+}
+
+// Функционал закрытия модального окна для кнопки крестика
+function closeModal() {
+    modalEl.classList.remove("modal--show");
+    document.body.classList.remove("stop-scrolling");
+}
+
+// Функционал закрытия модального окна при нажатии в зону вне окна
+window.addEventListener("click", (e) => {
+    if (e.target === modalEl) {
+        closeModal();
+    }
+})
+
+// Функционал закрытия модального окна при нажатии на кнопу ESC (в keyCode она имеет номер 27)
+window.addEventListener("keydown", (e) => {
+    if (e.keyCode === 27) {
+        closeModal();
+    }
+})
