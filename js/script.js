d4c549c2f57455af4bd43b6995c096d6eacae8f3
MidnightRoam <saldatsenka@list.ru>
2022-10-17 16:22:40 +0200

    css styles were expanded

--- js/script.js
+++ js/script.js
@@ -36,9 +36,9 @@ console.log(getUsers());
 // Модальное окно
 const modalEl = document.querySelector('.modal');
 
-// Функционал открытия модального окна и получения данных пользователей
-async function openModal(user_name) {
-    console.log(user_name)
+// Функционал открытия модального окна и получения личных данных пользователей
+async function openModal(user) {
+    console.log(user)
     let response = await fetch(`http://127.0.0.1:3000`);
     let content = await response.json();
 
@@ -46,24 +46,44 @@ async function openModal(user_name) {
     modalEl.classList.add('modal--show');
     document.body.classList.add("stop-scrolling");
 
-    modalEl.innerHTML = `
-    <div class="modal__card rounded">
-        <button type="button" class="modal__button-close">&#10006;</button>
-        <div class="user__name">${user_name.name}</div>
-        <div class="user__phone">Телефон: </div>
-        <div class="user__email">Почта: </div>
-        <div class="user__date">Дата приема: </div>
-        <div class="user__position-name">Должность: </div>
-        <div class="user__department">Подразделение: </div>
-        <div class="card__info">
-            <p><b>Дополнительная информация:</b></p>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam aperiam assumenda delectus 
-            distinctio harum neque numquam porro repudiandae sed? Eos numquam porro provident recusandae!
-            </p>
-        </div>
-    </div>   
-    `
+    for (let key in content) {
+        console.log(content[key].name);
+
+        modalEl.innerHTML = `
+            <div class="modal__card rounded">
+                <button type="button" class="modal__button-close">&#10006;</button>
+                <div class="modal__user-name"><b>${content[key].name}</b></div>
+                <ul class="modal__user-phone">
+                    <li class="li__main"><b>Телефон:</b></li>
+                    <li class="li__secondary">${content[key].phone}</li>
+                </ul>
+                <ul class="modal__user-email">
+                    <li class="li__main"><b>Почта:</b></li> 
+                    <li class="li__secondary">${content[key].email}</li>
+                </ul>
+                <ul class="modal__user-date">
+                    <li class="li__main"><b>Дата приема:</b></li>
+                    <li class="li__secondary">${content[key].hire_date}</li>
+                </ul>
+                <ul class="modal__user-position-name">
+                    <li class="li__main"><b>Должность:</b></li>
+                    <li class="li__secondary">${content[key].position_name}</li>
+                </ul>
+                <ul class="modal__user-department">
+                    <li class="li__main"><b>Подразделение:</b></li>
+                    <li class="li__secondary">${content[key].department}</li>
+                </ul>
+                <div class="card__info mg-10">
+                    <p class="info__title"><b>Дополнительная информация:</b></p>
+                    <p class="info__text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam aperiam assumenda delectus 
+                        distinctio harum neque numquam porro repudiandae sed? Eos numquam porro provident recusandae!
+                    </p>
+                </div>
+            </div>      
+        `
+    }
+    // При нажатии на крестик - закрываем модальное окно
     const btnClose = document.querySelector(".modal__button-close")
     btnClose.addEventListener("click", () => closeModal());
 }
