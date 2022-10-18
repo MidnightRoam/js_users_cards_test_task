be30fe106faae8c4ddff6389c8810da5af25c028
MidnightRoam <saldatsenka@list.ru>
2022-10-18 17:40:51 +0200

    Personal users popup windows were developed

--- js/script.js
+++ js/script.js
@@ -1,35 +1,70 @@
-// Функция получения данных пользователя с бэкенда и вывод пользотвательских карточек
-async function getUsers() {
-    let response = await fetch('http://127.0.0.1:3000');
-    let content = await response.json();
-    let user_list = document.querySelector('.page')
+// Loader
+responseJson('http://127.0.0.1:3000')
+let load = document.querySelector('.loader')
+let value;
+// Функция получения данных с сервера
+function responseJson(url) {
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json '
+        },
+    }).then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response error');
+            } else {
+                load.remove();
+                return response.json();
+            }
+        })
+        .then((data) => {
+            getUsers(data);
+        })
+        .catch((error) => {
+            console.error("Error", error);
+        })
+}
+
+// async function responseJson(url) {
+//     const response = await fetch(url);
+//     return response.json();
+// }
+
+// Функция вывода пользотвательских карточек
+function getUsers(content) {
+    // const content = responseJson('http://127.0.0.1:3000');
+    console.log(content);
+
+    let user_list = document.querySelector('.li__card')
 
     let key;
     for (key in content) {
         /* Добавляем в HTML персональные карточки пользователей */
         user_list.innerHTML += ` 
-            <ul class="ul">
-                <li>
-                    <div class="card__body rounded shadow">
-                        <div class="user__name">${content[key].name}</div>
-                        <div class="user__info pd-10">
-                        <div class="user__phone pd-10">
-                        <img 
-                        class='icon' 
-                        src="https://img.icons8.com/external-icongeek26-outline-gradient-icongeek26/344/external-phone-essentials-icongeek26-outline-gradient-icongeek26.png" 
-                        alt="phone">${content[key].phone}
+                        <div class="card__body rounded shadow mg-10" onclick="openModal(this)" id="xx${key}">
+                            <div class="user__name">${content[key].name}</div>
+                            <div class="user__info pd-10">
+                            <div class="user__phone pd-10">
+                            <img 
+                                class='icon' 
+                                src="https://img.icons8.com/external-icongeek26-outline-gradient-icongeek26/344/external-phone-essentials-icongeek26-outline-gradient-icongeek26.png" 
+                                alt="phone"
+                                >${content[key].phone}
+                            </div>
+                            <div class="user__email pd-10">
+                            <img class='icon' 
+                                src="https://img.icons8.com/external-bearicons-gradient-bearicons/344/external-Email-email-bearicons-gradient-bearicons.png" 
+                                alt="email"
+                                >${content[key].email}
+                            </div>
                         </div>
-                        <div class="user__email pd-10">
-                        <img class='icon' 
-                        src="https://img.icons8.com/external-bearicons-gradient-bearicons/344/external-Email-email-bearicons-gradient-bearicons.png" 
-                        alt="email">${content[key].email}
-                        </div>
-                    </div>
-                </li>
-            </ul>
         `
-        user_list.addEventListener("click", () => openModal(content[key].name))
     }
+    for (key in content) {
+        let y = document.querySelector('#xx' + key)
+        y.setAttribute('data-user', JSON.stringify(content[key]))
+    }
+    // user_list.addEventListener("click", () => openModal(content[key].name))
 }
 console.log(getUsers());
 
@@ -37,41 +72,37 @@ console.log(getUsers());
 const modalEl = document.querySelector('.modal');
 
 // Функционал открытия модального окна и получения личных данных пользователей
-async function openModal(user) {
-    console.log(user)
-    let response = await fetch(`http://127.0.0.1:3000`);
-    let content = await response.json();
-
+function openModal(obj) {
+    // let x = (obj).attribut(data-user)
     // При клике на карточку делаем модальное окно видимое и запрещаем скорллинг
     modalEl.classList.add('modal--show');
     document.body.classList.add("stop-scrolling");
-
-    for (let key in content) {
-        console.log(content[key].name);
-
+    console.log(obj.getAttribute('data-user'))
+    let x = JSON.parse(obj.getAttribute('data-user'))
+    // Вывод данных в модальном окне
         modalEl.innerHTML = `
-            <div class="modal__card rounded">
+            <div class="modal__card rounded" data-name="${x.name}">
                 <button type="button" class="modal__button-close">&#10006;</button>
-                <div class="modal__user-name"><b>${content[key].name}</b></div>
+                <div class="modal__user-name"><b>${x.name}</b></div>
                 <ul class="modal__user-phone">
                     <li class="li__main"><b>Телефон:</b></li>
-                    <li class="li__secondary">${content[key].phone}</li>
+                    <li class="li__secondary">${x.phone}</li>
                 </ul>
                 <ul class="modal__user-email">
                     <li class="li__main"><b>Почта:</b></li> 
-                    <li class="li__secondary">${content[key].email}</li>
+                    <li class="li__secondary">${x.email}</li>
                 </ul>
                 <ul class="modal__user-date">
                     <li class="li__main"><b>Дата приема:</b></li>
-                    <li class="li__secondary">${content[key].hire_date}</li>
+                    <li class="li__secondary">${x.hire_date}</li>
                 </ul>
                 <ul class="modal__user-position-name">
                     <li class="li__main"><b>Должность:</b></li>
-                    <li class="li__secondary">${content[key].position_name}</li>
+                    <li class="li__secondary">${x.position_name}}</li>
                 </ul>
                 <ul class="modal__user-department">
                     <li class="li__main"><b>Подразделение:</b></li>
-                    <li class="li__secondary">${content[key].department}</li>
+                    <li class="li__secondary">${x.department}</li>
                 </ul>
                 <div class="card__info mg-10">
                     <p class="info__title"><b>Дополнительная информация:</b></p>
@@ -80,9 +111,8 @@ async function openModal(user) {
                         distinctio harum neque numquam porro repudiandae sed? Eos numquam porro provident recusandae!
                     </p>
                 </div>
-            </div>      
+            </div>
         `
-    }
     // При нажатии на крестик - закрываем модальное окно
     const btnClose = document.querySelector(".modal__button-close")
     btnClose.addEventListener("click", () => closeModal());
@@ -117,7 +147,7 @@ window.onload = () => {
 
         if(value) {
             list.forEach(elem => {
-               if (elem.innerText.search(value) == -1) {
+               if (elem.innerText.search(value) === -1) {
                    elem.classList.add('hide');
                }
             });
